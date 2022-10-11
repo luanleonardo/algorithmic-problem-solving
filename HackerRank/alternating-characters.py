021fe7e219d9163d5c7826bd554c35448bdc4b39
Luan <luan.moraes@loggi.com>
2022-10-10 21:27:39 -0300

    add HackerRank/alternating-characters.py

---
+++ HackerRank/alternating-characters.py
@@ -0,0 +1,22 @@
+from sys import stdin, stdout
+
+
+def alternatingCharacters(s):
+    n = len(s)
+    if n == 1:
+        return 0
+
+    lv = alternatingCharacters(s[: n // 2])
+    rv = alternatingCharacters(s[n // 2 :])
+
+    if s[: n // 2][-1] == s[n // 2 :][0]:
+        return lv + rv + 1
+
+    return lv + rv
+
+
+q = int(stdin.readline().rstrip())
+
+for _ in range(q):
+    s = stdin.readline().rstrip()
+    stdout.write(f"{alternatingCharacters(s)}\n")
