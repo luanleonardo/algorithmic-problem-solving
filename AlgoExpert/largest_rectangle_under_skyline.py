46684e87e8771e2f4cbaa58409dfeab9b70c4340
Luan <luan.moraes@loggi.com>
2022-11-14 14:07:13 -0300

    add largest-rectangle-under-skyline.py

---
+++ AlgoExpert/largest_rectangle_under_skyline.py
@@ -0,0 +1,44 @@
+from collections import deque, namedtuple
+from sys import stdin, stdout
+
+
+# O(num_buildings) time | O(num_buildings) space
+def largest_rectangle_using_stack(buildings, num_buildings):
+    Candidate = namedtuple("Candidate", ["index", "height"])
+    left_candidates = deque([])
+    largest_area = 0
+    for right in range(num_buildings):
+        height = buildings[right]
+        next_left = right
+        while left_candidates and left_candidates[-1].height >= height:
+            candidate = left_candidates.pop()
+            width = right - candidate.index
+            area = width * candidate.height
+            largest_area = max(largest_area, area)
+            next_left = candidate.index
+        left_candidates.append(Candidate(next_left, height))
+    return largest_area
+
+
+# O(num_buildings^2) time | O(1) space
+def largest_rectangle(buildings, num_buildings):
+    largest_area = 0
+    for left in range(num_buildings):
+        height = buildings[left]
+        for right in range(left, num_buildings):
+            width = right - left + 1
+            height = min(height, buildings[right])
+            area = width * height
+            largest_area = max(largest_area, area)
+    return largest_area
+
+
+def largestRectangleUnderSkyline(buildings):
+    buildings.append(0)
+    num_buildings = len(buildings)
+    return largest_rectangle_using_stack(buildings, num_buildings)
+
+
+if __name__ == "__main__":
+    buildings = [1, 3, 3, 2, 4, 1, 5, 3, 2]
+    stdout.write(f"{largestRectangleUnderSkyline(buildings)}\n")
