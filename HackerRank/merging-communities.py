f5559883f74dfe0cfb163bc87b922f45c13e54f0
Luan <luan.moraes@loggi.com>
2022-12-24 13:46:03 -0300

    add merging-communities.py

---
+++ HackerRank/merging-communities.py
@@ -0,0 +1,40 @@
+# https://www.hackerrank.com/challenges/merging-communities
+
+from sys import stdin, stdout
+
+
+class UnionFind:
+    def __init__(self, n) -> None:
+        self.parent = [i for i in range(n + 1)]
+        self.size = [1 for _ in range(n + 1)]
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        u = self.parent[u]
+        v = self.parent[v]
+        if u == v:
+            return
+        if self.size[u] > self.size[v]:
+            u, v = v, u
+        self.parent[u] = v  # size[u] <= size[v]
+        self.size[v] += self.size[u]
+
+
+if __name__ == "__main__":
+    n, n_qry = map(int, stdin.readline().strip().split())
+
+    uf = UnionFind(n)
+    for _ in range(n_qry):
+        qry = list(stdin.readline().strip().split())
+
+        u = int(qry[1])
+        if qry[0] == "M":
+            v = int(qry[2])
+            uf.union(u, v)
+        else:
+            u = uf.find(u)
+            stdout.write(f"{uf.size[u]}\n")
