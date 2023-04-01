1a7b4b88feefb4d435397508f2b1c3bf73183fd3
Luan <luan.moraes@loggi.com>
2023-04-01 12:27:40 -0300

    update AlgoExpert/minimum_waiting_time.py

--- AlgoExpert/minimum_waiting_time.py
+++ AlgoExpert/minimum_waiting_time.py
@@ -5,9 +5,9 @@
 def minimumWaitingTime(queries):
     queries.sort()
     waiting_time = 0
-    total_queries = len(queries)
+    num_queries = len(queries)
     for i, duration in enumerate(queries, start=1):
-        waiting_time += duration * (total_queries - i)
+        waiting_time += duration * (num_queries - i)
     return waiting_time
 
 
