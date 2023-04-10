ce3d3cd53b3f30b7c1f75b68d8b756e7a8c96c75
Luan <luan.moraes@loggi.com>
2023-04-10 19:20:21 -0300

    update AlgoExpert/node_depths.py and test

--- AlgoExpert/node_depths.py
+++ AlgoExpert/node_depths.py
@@ -9,16 +9,28 @@ class BinaryTree:
         self.right = None
 
 
-def nodeDepthsSum(node, running_sum, total_sum):
-    if node is None:
-        return
-    total_sum.append(running_sum)
-    nodeDepthsSum(node.left, running_sum + 1, total_sum)
-    nodeDepthsSum(node.right, running_sum + 1, total_sum)
-
-
+# def nodeDepthsSum(node, running_sum, total_sum):
+#     if node is None:
+#         return
+#     total_sum.append(running_sum)
+#     nodeDepthsSum(node.left, running_sum + 1, total_sum)
+#     nodeDepthsSum(node.right, running_sum + 1, total_sum)
+#
+#
 # O(n) time | O(n) space, n = number of nodes
-def nodeDepths(root):
-    total_sum = []
-    nodeDepthsSum(node=root, running_sum=0, total_sum=total_sum)
-    return sum(total_sum)
+# def nodeDepths(root):
+#     total_sum = []
+#     nodeDepthsSum(node=root, running_sum=0, total_sum=total_sum)
+#     return sum(total_sum)
+
+
+# O(n) time | O(h) space, n = number of nodes, h = tree height
+def nodeDepths(node, depth=0):
+    if node is None:
+        return 0
+
+    return (
+        depth
+        + nodeDepths(node=node.left, depth=depth + 1)
+        + nodeDepths(node=node.right, depth=depth + 1)
+    )
