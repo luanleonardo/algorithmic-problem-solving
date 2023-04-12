f4d69c5169f913bb9cb933aa317f5ffb82fee88f
Luan <luan.moraes@loggi.com>
2023-04-12 14:40:32 -0300

    add AlgoExpert/depth_first_search.py and test

---
+++ AlgoExpert/depth_first_search.py
@@ -0,0 +1,38 @@
+# https://www.algoexpert.io/questions/depth-first-search
+from collections import deque
+
+
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    # O(v + e) time | O(v) space, v = number of vertices, e = number of edges
+    # def depthFirstSearch(self, array):
+    #     explored = {id(self)}
+    #     frontier = deque([self])
+    #     while frontier:
+    #         node = frontier.pop()
+    #         array.append(node.name)
+    #         for child in node.children[::-1]:
+    #             if id(child) in explored:
+    #                 continue
+    #             explored.add(id(child))
+    #             frontier.append(child)
+    #     return array
+
+    # O(v + e) time | O(v) space, v = number of vertices, e = number of edges
+    def depthFirstSearch(self, array, explored=None):
+        if explored is None:
+            explored = set()
+        array.append(self.name)
+        explored.add(id(self))
+        for child in self.children:
+            if id(child) in explored:
+                continue
+            child.depthFirstSearch(array, explored)
+        return array
---
+++ AlgoExpert/tests/test_depth_first_search.py
@@ -0,0 +1,20 @@
+import pytest
+
+from AlgoExpert.depth_first_search import Node
+
+
+@pytest.fixture
+def graph_tree():
+    graph = Node("A")
+    graph.addChild("B").addChild("C").addChild("D")
+    graph.children[0].addChild("E").addChild("F")
+    graph.children[2].addChild("G").addChild("H")
+    graph.children[0].children[1].addChild("I").addChild("J")
+    graph.children[2].children[0].addChild("K")
+    return graph
+
+
+def test_dfs(graph_tree):
+    actual = ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"]
+    expected = graph_tree.depthFirstSearch([])
+    assert actual == expected
