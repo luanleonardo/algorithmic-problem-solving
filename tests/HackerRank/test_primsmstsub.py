ed686cb7e9b929354a6795b2305581b4fbe4e71c
Luan <luan.moraes@loggi.com>
2023-04-19 19:13:00 -0300

    add test for HackerRank/primsmstsub.py

--- tests/HackerRank/test_primsmstsub.py
+++ tests/HackerRank/test_primsmstsub.py
@@ -1,12 +1,22 @@
-"""
-5 6
-1 2 3
-1 3 4
-4 2 6
-5 2 2
-2 3 5
-3 5 7
-1
-"""
+from HackerRank.primsmstsub import prims
+
+
 def test_prim_heap_based_algo():
-    assert True
+    """
+    Test Case:
+    5 6
+    1 2 3
+    1 3 4
+    4 2 6
+    5 2 2
+    2 3 5
+    3 5 7
+    1
+    """
+    n, m = 5, 6
+    edges = [[1, 2, 3], [1, 3, 4], [4, 2, 6], [5, 2, 2], [2, 3, 5], [3, 5, 7]]
+    start = 1
+
+    expected = 15
+    min_cost, mst = prims(n=n, edges=edges, start=start)
+    assert expected == min_cost
