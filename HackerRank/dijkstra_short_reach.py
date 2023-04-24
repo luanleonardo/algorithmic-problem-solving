77ff8d7bfd073f9121b231e599b0f95331f263bb
Luan <luan.moraes@loggi.com>
2023-04-24 12:35:47 -0300

    add HackerRank/dijkstra_short_reach.py

---
+++ HackerRank/dijkstra_short_reach.py
@@ -0,0 +1,51 @@
+# https://www.hackerrank.com/challenges/dijkstrashortreach/problem?isFullScreen=false
+
+# from collections import defaultdict
+from heapq import heappop, heappush
+# from sys import stdin, stdout
+
+
+def dijkstra(graph, start):
+    processed = set()
+    distances = [float("inf")] * len(graph)
+    distances[start] = 0
+    heap = [(distances[start], start)]
+    while heap:
+        dist_node, node = heappop(heap)
+        if node in processed:
+            continue
+        for neighbor in graph[node]:
+            dist_neighbor = dist_node + graph[node][neighbor]
+            if dist_neighbor < distances[neighbor]:
+                distances[neighbor] = dist_neighbor
+                heappush(heap, (distances[neighbor], neighbor))
+    return distances
+
+
+# if __name__ == "__main__":
+#     t = int(stdin.readline().strip())
+#     for i in range(t):
+#         n, m = map(int, stdin.readline().strip().split())
+#         g = defaultdict(dict)
+#         for u in range(n):
+#             g[u] = dict()
+#         for _ in range(m):
+#             u, v, w = map(int, stdin.readline().strip().split())
+#             u, v = u - 1, v - 1
+#             if v in g[u] or u in g[v]:
+#                 g[u][v] = min(g[u][v], w)
+#                 g[v][u] = min(g[v][u], w)
+#             else:
+#                 g[u][v] = w
+#                 g[v][u] = w
+#         s = int(stdin.readline().strip()) - 1
+#         answer = ""
+#         for distance in dijkstra(graph=g, start=s):
+#             if distance == 0:
+#                 continue
+#             if distance == float("inf"):
+#                 answer += "-1 "
+#             else:
+#                 answer += f"{distance} "
+#         answer.strip()
+#         stdout.write(answer + "\n")
