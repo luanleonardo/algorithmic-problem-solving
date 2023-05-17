b467f9acc0e542fab3496741f51a9f108060173d
Luan <luan.moraes@loggi.com>
2023-05-17 16:18:43 -0300

    add AlgoExpert/min_number_of_coins_for_change.py

--- AlgoExpert/min_number_of_coins_for_change.py
+++ AlgoExpert/min_number_of_coins_for_change.py
@@ -1,16 +1,24 @@
-def min_coin_change(x, capacity):
-    num_items = len(x)
-    dp = [[0 for _ in range(capacity + 1)] for _ in range(num_items + 1)]
-    for c in range(1, capacity + 1):
-        for i in range(1, num_items + 1):
-            if x[i - 1] <= c:
-                dp[i][c] = max(
-                    dp[i][c], dp[i][c - x[i - 1]] + x[i - 1]
-                )
-    import pdb; pdb.set_trace()
-    return dp[num_items][capacity]
+# https://www.algoexpert.io/questions/min-number-of-coins-for-change
+
+
+def min_coin_change(x, R):
+    n = len(x)
+    dp = [[float("inf") for _ in range(R + 1)] for _ in range(n)]
+
+    for m in range(R + 1):
+        if m % x[0] == 0:
+            dp[0][m] = m // x[0]
+
+    for i in range(1, n):
+        for m in range(R + 1):
+            if x[i] <= m and dp[i][m - x[i]] + 1 < dp[i - 1][m]:
+                dp[i][m] = dp[i][m - x[i]] + 1
+            else:
+                dp[i][m] = dp[i - 1][m]
+
+    return dp[n - 1][R]
 
 
 def minNumberOfCoinsForChange(n, denoms):
-    # Write your code here.
-    return min_coin_change(x=denoms, capacity=n)
+    result = min_coin_change(x=denoms, R=n)
+    return result if result != float("inf") else -1
